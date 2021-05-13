0f206eaf4a41341b9c2bb649d3e3e16fc8b2f5af
nataly garcia <naty_garcioli@hotmail.com>
2021-05-13 15:45:45 -0500

    added routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
+  
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  namespace :api, defaults: { format: :json } do
+    namespace :v1 do
+      resources :characters, only: [ :index, :show, :update, :create, :destroy]
+      resources :movies, only: [ :index, :show, :update, :create, :destroy ]
+      resources :series, only: [ :index, :show, :update, :create, :destroy ]
+    end
+  end
 end
