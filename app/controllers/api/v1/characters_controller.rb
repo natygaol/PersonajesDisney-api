216ab285c4a2a577416cb0278d3faaa4c5639ec5
nataly garcia <naty_garcioli@hotmail.com>
2021-05-14 18:01:47 -0500

    added token

--- app/controllers/api/v1/characters_controller.rb
+++ app/controllers/api/v1/characters_controller.rb
@@ -1,4 +1,5 @@
 class Api::V1::CharactersController < Api::V1::BaseController
+    acts_as_token_authentication_handler_for User, except: [ :index, :show ]
     before_action :set_character, only: [ :show ]
   
       def index
